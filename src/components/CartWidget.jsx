6ec5253f3a4e09dbb71d4efa4b4bfa3b38da8f63
Lueguino <lucyeguino@gmail.com>
2024-07-10 17:29:46 -0700

    first commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,7 @@
+import carrito from '../assets/carrito.jpg';
+
+export const CartWidget = () => (
+<>
+<img src={carrito} style={{ width: '150px', height: '150px'}}/>9
+</>
+);
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => (
+ <>
+    <h2>Salsa marisquera</h2>
+    <ul>
+    <li><a href="">Salsa negra</a></li>
+    <li><a href="">Salsa roja</a></li>
+    <li><a href="">Salsa verde</a></li>
+    <li><a href="">Extras</a></li>
+  </ul>
+  <CartWidget/>  
+ </>
+);
